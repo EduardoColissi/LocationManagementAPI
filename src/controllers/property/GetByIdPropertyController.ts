2000b3f17c8b18e774adff1d40569276ed6f4ade
Eduardo Colissi <colissi.eduardo@gmail.com>
2023-09-09 20:49:48 -0300

    feat: get by id property controller

---
+++ src/controllers/property/GetByIdPropertyController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { prismaClient } from "../../database/prismaClient";
+
+export class GetByIdPropertyController {
+  async handle(req: Request, res: Response): Promise<void> {
+    try {
+      if (!req.user) {
+        res.status(401).json({
+          errors: [
+            { message: "Não autorizado! Por favor, realize seu login." },
+          ],
+        });
+        return;
+      }
+
+      const user_id = Number(req.user.id);
+      const property_id = Number(req.params.id);
+
+      const property = await prismaClient.property.findUnique({
+        where: {
+          id: property_id,
+          user_id: user_id,
+        },
+      });
+
+      res.status(property != null ? 200 : 204).json({
+        message:
+          property != null
+            ? `Imóvel encontrado com sucesso!`
+            : "Imóvel não encontrado.",
+        property: property,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: [{ message: "Erro interno de servidor. " }] });
+    }
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -10,12 +10,16 @@ import { LoginController } from "./controllers/user/LoginController";
 import { CreatePropertyController } from "./controllers/property/CreatePropertyController";
 import { DeletePropertyController } from "./controllers/property/DeletePropertyController";
 import { GetAllPropertiesController } from "./controllers/property/GetAllPropertiesController";
+import { GetByIdPropertyController } from "./controllers/property/GetByIdPropertyController";
+import { EditPropertyController } from "./controllers/property/EditPropertyController";
 
 const signup = new SignupController();
 const login = new LoginController();
 const createProperty = new CreatePropertyController();
 const deleteProperty = new DeletePropertyController();
 const getAllProperties = new GetAllPropertiesController();
+const getByIdProperty = new GetByIdPropertyController();
+const editProperty = new EditPropertyController();
 
 const routes = Router();
 
@@ -27,5 +31,7 @@ routes.use(auth);
 routes.post("/property", propertyValidator, createProperty.handle);
 routes.delete("/property/:id", deleteProperty.handle);
 routes.get("/properties", getAllProperties.handle);
+routes.get("/property/:id", getByIdProperty.handle);
+routes.put("/property/:id", propertyValidator, editProperty.handle);
 
 export default routes;
