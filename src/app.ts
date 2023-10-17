20e3e5d8925850b1cbfeda9bbba3310dd1fd2df2
Eduardo Colissi <colissi.eduardo@gmail.com>
2023-10-17 02:21:51 -0300

    fix: change some configs

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT ? Number(process.env.PORT) : 4000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
